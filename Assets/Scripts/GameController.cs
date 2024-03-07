7142e0dee7ac12221eee7436f948f100d7b23082
Vergur <elovikovgri@yandex.ru>
2024-03-08 01:46:20 +0300

    feat: add scorestreak saver

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using UnityEngine;
 using TMPro;
-using UnityEngine;
-
 
 public class GameController : MonoBehaviour
 {
@@ -10,16 +8,20 @@ public class GameController : MonoBehaviour
     [SerializeField] private Plate _enemyPlate;
     [SerializeField] private TextMeshProUGUI _playerScoreText;
     [SerializeField] private TextMeshProUGUI _enemyScoreText;
+    [SerializeField] private TextMeshProUGUI _bestScoreText; 
+    [SerializeField] private TextMeshProUGUI _currentScoreText; 
 
     private int _playerScore;
     private int _enemyScore;
+    private int _currentScoreStreak;
+    private int _bestScore; 
     
     public enum PlayerType
     {
         Gamer,
         Enemy
     }
-
+    
     private void Awake()
     {
         EventsSystem.OnPlayerScored += OnScored;
@@ -32,9 +34,10 @@ public class GameController : MonoBehaviour
 
     private void StartGame()
     {
-        SetPlayerScore(0, _playerScoreText);
-        SetPlayerScore(0, _enemyScoreText);
+        SetPlayerScore(0);
+        SetEnemyScore(0);
         StartRound();
+        GetBestScore();
     }
 
     private void StartRound()
@@ -47,15 +50,48 @@ public class GameController : MonoBehaviour
 
     private void OnScored(PlayerType playerType)
     {
-        SetPlayerScore(playerType == PlayerType.Gamer ? ++_playerScore : ++_enemyScore, playerType == PlayerType.Gamer ? _playerScoreText : _enemyScoreText);
+        if (playerType == PlayerType.Enemy)
+        {
+            SetPlayerScore(++_playerScore);
+            _currentScoreText.SetText($"Scorestreak:{++_currentScoreStreak}");
+            if (_currentScoreStreak > _bestScore) SaveBestScore(_currentScoreStreak);
+        }
+        else
+        {
+            SetEnemyScore(++_enemyScore);
+        }
+
         StartRound();
     }
     
-    private void SetPlayerScore(int score, TextMeshProUGUI playerScoreText)
+    private void GetBestScore()
     {
-        playerScoreText.SetText(score.ToString());
+        _bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        _bestScoreText.SetText($"Best score: {_bestScore.ToString()}");
+        _currentScoreText.SetText($"Scorestreak:{0}");
     }
-    
+
+    private void SaveBestScore(int score)
+    {
+        _bestScore = score;
+        PlayerPrefs.SetInt("BestScore", _bestScore); 
+        _bestScoreText.SetText($"Best score: {_bestScore.ToString()}");
+    }
+
+    private void SetPlayerScore(int score)
+    {
+        _playerScore = score;
+        _playerScoreText.SetText(score.ToString());
+    }
+
+    private void SetEnemyScore(int score)
+    {
+        _enemyScore = score;
+        _enemyScoreText.SetText(score.ToString());
+        _currentScoreStreak = 0;
+        _currentScoreText.SetText($"Scorestreak:{_currentScoreStreak}");
+    }
+
     private void OnDestroy()
     {
         EventsSystem.OnPlayerScored -= OnScored;
